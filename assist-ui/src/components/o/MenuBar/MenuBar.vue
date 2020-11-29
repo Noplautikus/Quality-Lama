a5a1bc5675f1463916a7cab5d0beb05a1960479f
Philipp <philipp.gramig@gmail.com>
2020-11-29 20:42:19 +0100

    Added button functions on menubar

--- assist-ui/src/components/o/MenuBar/MenuBar.vue
+++ assist-ui/src/components/o/MenuBar/MenuBar.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="menu-bar">
+      <div class="minimize-button">
+          <i class="el-icon-minus" @click="minimizeWindow"></i>
+      </div>
+      <div class="close-button">
+          <i class="el-icon-close" @click="closeWindow"></i>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+const { remote } = require('electron');
 
+export default {
+  setup() {
+    function minimizeWindow() {
+      remote.getCurrentWindow().minimize();
+    }
+
+    function closeWindow() {
+      remote.getCurrentWindow().destroy();
+    }
+
+    return {
+      minimizeWindow,
+      closeWindow,
+    };
+  },
 };
 </script>
 
@@ -16,5 +37,25 @@ export default {
   background-color: $bg-dark-main;
   margin: -8px 0 0 -8px;
   -webkit-app-region: drag;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  .minimize-button {
+    align-self: flex-end;
+  }
 }
+
+i {
+    color: $text-main;
+    font-size: 23px;
+    margin-right: 8px;
+    -webkit-app-region: no-drag;
+
+    &:hover {
+      cursor: pointer;
+      color: $text-main-hover;
+    }
+}
+
 </style>
