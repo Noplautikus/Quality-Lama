6189af32fe2fdf85ab46b96a03eaacebb01b7afe
Philipp <philipp.gramig@gmail.com>
2020-12-08 19:04:23 +0100

    Set resizable to false

--- quality-lama/src/background.js
+++ quality-lama/src/background.js
@@ -19,6 +19,7 @@ async function createWindow() {
     width: 800,
     height: 500,
     frame: false,
+    resizable: false,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
