528b31854a86ecf865f568206ba52afcb15f6c54
Philipp <philipp.gramig@gmail.com>
2021-01-06 01:37:44 +0100

    Added costum number input

---
+++ quality-lama/src/components/a/CustomNumberInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-input class="custom-number-input" :placeholder="placeholder" type="number"></el-input>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      required: false,
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.custom-number-input {
+  width: 200px;
+  display: block;
+
+  .el-input__inner {
+    background-color: $bg-dark-third;
+    border: 1px solid $bg-dark-main;
+    color: $text-main;
+  }
+}
+</style>
