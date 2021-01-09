b58eb2a0d150d46bba10dd4fc6495209c3c64169
Philipp <philipp.gramig@gmail.com>
2021-01-09 23:39:00 +0100

    Added new file path

--- quality-lama/src/constants/filePaths.js
+++ quality-lama/src/constants/filePaths.js
@@ -2,7 +2,9 @@ const os = require('os');
 const path = require('path');
 
 const USER_SETTINGS_FILE_PATH = path.join(os.homedir(), 'AppData', 'Roaming', 'quality-lama', 'userSettings.json');
+const BILL_FILE_PATH = path.join(os.homedir(), 'AppData', 'Roaming', 'quality-lama', 'bills.json');
 
 module.exports = {
   USER_SETTINGS_FILE_PATH,
+  BILL_FILE_PATH,
 };
