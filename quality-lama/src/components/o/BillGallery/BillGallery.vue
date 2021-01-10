fa8bd125716b0e315923cc2d35fcaac582b969d8
Philipp <philipp.gramig@gmail.com>
2021-01-10 01:17:20 +0100

    Added display of bills in billgalery

--- quality-lama/src/components/o/BillGallery/BillGallery.vue
+++ quality-lama/src/components/o/BillGallery/BillGallery.vue
@@ -1,14 +1,70 @@
 <template>
   <span class="heading">Alle Rechnungen</span>
+  <div class="bills">
+    <div class="bill-display" v-for="(bill, index) in bills" :key="index">
+      <i class="el-icon-document"></i>
+      <span>{{bill.billNumber}}</span>
+      <span>{{bill.date}}</span>
+      <span>{{bill.costumer.name}}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { ref, onMounted } from 'vue';
+import { useStore } from 'vuex';
 
+export default {
+  setup() {
+    const store = useStore();
+    const bills = ref([]);
+
+    function getBills() {
+      bills.value = store.getters.getBills;
+    }
+
+    onMounted(() => {
+      getBills();
+    });
+
+    return {
+      bills,
+    };
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.bills {
+  display: flex;
+  flex-wrap: wrap;
+
+  .bill-display {
+    cursor: pointer;
+    width: 130px;
+    border: 2px solid;
+    border-color: $text-main;
+    padding: 10px;
+    border-radius: 20px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+
+    &:hover {
+      background-color: $bg-dark-third;
+    }
+
+    i {
+      font-size: 80px;
+    }
+  }
+
+  span {
+    color: $text-main;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+}
 </style>
 
 //zentrierter text: doc.text('The text', doc.internal.pageSize.width/2, 30, null, null, 'center');
---
+++ quality-lama/src/components/o/BillGallery/__tests__/billgallery.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-duplicates */
+import { mount } from '@vue/test-utils';
+import store from '@/store';
+import ElementPlus from 'element-plus';
+import BillGallery from '../BillGallery.vue';
+
+const billStore = [{ billNumber: '1' }, { billNumber: '2' }, { billNumber: '3' }, { billNumber: '4' }];
+store.commit('OVERRIDE_BILL_STORE', billStore);
+
+const wrapper = mount(BillGallery, {
+  global: {
+    plugins: [store, ElementPlus],
+  },
+});
+const { vm } = wrapper;
+
+describe('billGallery', () => {
+  test('should store billStore to local varibale', () => {
+    expect(vm.bills).toEqual(billStore);
+  });
+});
